6841a2023bbaeb2ca437279836c9cd943a8396ee
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-10 16:01:13 +0100

    Fix syntax error in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
