9e5bbd8e57452b1235199d442a525727dab786e3
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-12 08:42:17 +0100

    Rename App to MyApp and include Head component
    Refactor App component to MyApp and add Head for metadata.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>MedyArt</title>
+        <link rel="icon" href="/icon.jpeg" />
+        <link rel="shortcut icon" href="/icon.jpeg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
